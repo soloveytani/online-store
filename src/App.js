5c78f32e93963f2d0d1018f8741a119dd9982bd0
Tania <soloveystn@gmail.com>
2019-02-27 15:51:36 +0200

    products-carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
+import "../node_modules/slick-carousel/slick/slick.css";
+import "../node_modules/slick-carousel/slick/slick-theme.css";
 import HomePage from './HomePage/HomePage';
 
 class App extends Component {
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,21 +2,50 @@ import React from 'react';
 import './HomePage.scss';
 import IPhone from './i_love_phone.svg';
 import PhonePanel from './maxresdefault.jpg';
+import Slider from "react-slick";
 
-const samplePhone = {
+const iphone8 = {
     imgUrl: 'https://img.mvideo.ru/Pdb/30030153b.jpg',
     name: 'Iphone 8',
     description: '64 gb Gold (MQ6J2RU/A)',
     cost: '44990 руб.'
 }
 
+const iphoneXSMax = {
+    imgUrl: 'https://img.mvideo.ru/Pdb/30040031b.jpg',
+    name: 'Iphone XS Max',
+    description: '512GB Gold (MT582RU/A)',
+    cost: '117990 руб.'
+}
+
+const iphoneXRY = {
+    imgUrl: 'https://img.mvideo.ru/Pdb/30040041b.jpg',
+    name: 'iPhone XR',
+    description: '128GB Yellow (MRYF2RU/A)',
+    cost: '64990 руб.'
+}
+
+const iphoneXRC = {
+    imgUrl: 'https://img.mvideo.ru/Pdb/30039935b.jpg',
+    name: 'iPhone XR',
+    description: '256GB Coral (MRYP2RU/A)',
+    cost: '70990 руб.'
+}
+
+const iphoneX = {
+    imgUrl: 'https://img.mvideo.ru/Pdb/30030163b.jpg',
+    name: 'iPhone X',
+    description: '64GB Space Gray (MQAC2RU/A)',
+    cost: '63990 руб.'
+}
+
 const samplePhones = [
-    samplePhone,
-    samplePhone,
-    samplePhone,
-    samplePhone,
-    samplePhone,
-    samplePhone
+    iphone8,
+    iphoneXSMax,
+    iphoneXRY,
+    iphoneXRC,
+    iphoneX,
+    iphone8
 ];
 
 const Card = (props) => (
@@ -30,6 +59,13 @@ const Card = (props) => (
 
 class HomePage extends React.Component {
     render() {
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4
+          };
         return (
             <div>   
                 <header className="hp-header">
@@ -43,11 +79,16 @@ class HomePage extends React.Component {
                     <div className="hp-standart-width hp-take-yours">Возьми своё, не стесняйся</div>
                 </section>
                 <section className="hp-products-section">
+                    <div className="hp-standart-width hp-section-label">Самые популярные продукты</div>
                     <div className="hp-standart-width products-list">
-                        { samplePhones.map((value, index) =>
-                            <Card phoneInfo={ value } key={index}/> 
-                        ) }
+                        <Slider {...settings}>
+                            { samplePhones.map((value, index) =>
+                                <Card phoneInfo={ value } key={index}/> 
+                            ) }
+                        </Slider>
                     </div>
+                    
+      
                 </section>
             </div>
             
